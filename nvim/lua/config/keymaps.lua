1be91b9616f4be73e2ab26f79edfde763b8315f5
Hiren Patel <imhpdev.social@gmail.com>
2025-05-15 17:52:51 -0400

    Neovim: base setup + lazy, kanagawa, oil.nvim, mini.statusline, sleuth-vim

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,2 @@
+-- keymap for Oil file explorer
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "open parent directory in Oil" })
---
+++ nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,13 @@
+return {
+	"rebelot/kanagawa.nvim",
+	config = function()
+		require("kanagawa").setup({
+			compile=true,
+			transparent=false
+		});
+		vim.cmd("colorscheme kanagawa");
+	end,
+	build = function()
+		vim.cmd("KaganawaCompile");
+	end,
+}
